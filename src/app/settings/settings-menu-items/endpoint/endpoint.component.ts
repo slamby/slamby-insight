0c76457aeaff324339415ae84872f7167f607668
Béla Soós <bela.soos@slamby.com>
2016-10-28 10:13:18 +0200

    correct invalid apiBaseEndPoint

--- src/app/settings/settings-menu-items/endpoint/endpoint.component.ts
+++ src/app/settings/settings-menu-items/endpoint/endpoint.component.ts
@@ -49,7 +49,9 @@ export class EndpointComponent implements AfterContentInit {
 
     save() {
         let reload = this.equals(this.pendingEndpoint, this.selectedEndpoint);
-
+        if (this.pendingEndpoint.ApiBaseEndpoint.endsWith("/")) {
+            this.pendingEndpoint.ApiBaseEndpoint = this.pendingEndpoint.ApiBaseEndpoint.slice(0, -1);
+        }
         if (this.pendingEndpoint.Id) {
             let index = this.endpoints.indexOf(this.endpoints.find(e => e.Id === this.pendingEndpoint.Id));
             this.endpoints[index] = this.pendingEndpoint;
